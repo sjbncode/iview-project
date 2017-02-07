520cb5f5ffc38c02e216c08c69b6b85309e91e43
sijian.gu <sijian.gu@genewiz.com>
2017-02-07 17:09:22 +0800

    init

--- src/components/app.vue
+++ src/components/app.vue
@@ -1,12 +1,39 @@
 <style scoped>
     @import '../styles/common.css';
+    .wrapper{
+        background-color: #eee;
+        padding: 1px;
+    }
+    .wrapper-header{
+        height: 30px;
+    }
+    .wrapper-navigate{
+        float: left;
+        height: 0px;
+    }
+    .wrapper-container {
+    background: #fff;
+    padding: 10px;
+    margin:0px 5px 10px 240px;
+    border-radius: 6px;
+    box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
+    }
 </style>
 <template>
-    <div>
-        <router-view></router-view>
+    <div class="wrapper">
+        <div class="wrapper-header"></div>
+        <div class="wrapper-navigate">
+            <!-- <img src="../images/logo.png">     -->
+            <mainnav></mainnav>
+        </div>
+        <div class="wrapper-container">
+            <router-view></router-view>
+        </div>
     </div>
+    <div class="footer"></div>
 </template>
 <script>
+    import mainnav from './mainnav.vue';
     export default {
         data () {
             return {}
@@ -19,6 +46,7 @@
         },
         methods: {
 
-        }
+        },
+        components:{mainnav}
     }
 </script>
--- src/router.js
+++ src/router.js
@@ -6,6 +6,30 @@ const routers = {
         component (resolve) {
             require(['./views/index.vue'], resolve);
         }
+    },
+
+    '/t/form': {
+        component (resolve) {
+            require(['./views/t/form.vue'], resolve);
+        }
+    },
+
+    '/t/form1': {
+        component (resolve) {
+            require(['./views/t/form1.vue'], resolve);
+        }
+    },
+
+    '/t/form2': {
+        component (resolve) {
+            require(['./views/t/form2.vue'], resolve);
+        }
+    },
+
+    '/t/form3': {
+        component (resolve) {
+            require(['./views/t/form3.vue'], resolve);
+        }
     }
 };
 export default routers;
